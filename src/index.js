1232dd4ff2a2e87eb5ae3448ffb5dee6826c3a6d
Zheka Medniy <madman5491@gmail.com>
2023-02-27 21:28:58 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let x = Math.abs(n)
+    if (x % 10 === 0) x = x / 10
+    x = x.toString().split('').reverse().join('')
+    return x
 }
